9e675f2e1eb732ed079e4eb5de814b58050bc6b2
wangtao <wangtao.cpu@gmail.com>
2023-02-10 14:23:22 +0800

    chroe: TDesign

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 </script>
 
 <template>
+    <router-view>
+        <!--App组件作为页面容器-->
+    </router-view>
 </template>
 
 <style scoped lang='less'>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router'
+import TDesign from 'tdesign-vue-next'
+import 'tdesign-vue-next/es/style/index.css'
+// 考虑到产物大小可以按需引入或者通过插件引入
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(TDesign).mount("#app")
