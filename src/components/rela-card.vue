0e09a4a2aed1f3eae8cf8aa87c8963f3a843e02c
wangtao <wangtao.cpu@gmail.com>
2023-05-03 20:21:28 +0800

    feta: menu drawer

--- src/components/rela-card.vue
+++ src/components/rela-card.vue
@@ -192,7 +192,7 @@ const deleteRela = (id: string) => {
             </div>
             <t-button class="add-rela" @click="addRela" theme="primary">
                 <template #icon><t-icon name="add" /></template>
-                新增关系
+                <div class="t">新增关系</div>
             </t-button>
             <t-dialog style="user-select: none;" v-model:visible="visibleModal" header="新增关系" mode="modal" draggable
                 :on-confirm="dialogConfim">
@@ -241,5 +241,11 @@ const deleteRela = (id: string) => {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+}
+
+.t {
+    @media screen and (max-width: 900px) {
+        display: none;
+    }
 }
 </style>
--- src/views/content/home.vue
+++ src/views/content/home.vue
@@ -1,5 +1,5 @@
 <template>
-    <t-layout>
+    <t-layout class="content">
         <t-card  title="数据概览" class="data">
         </t-card>
         <t-card  title="当前得分" class="data">
@@ -8,9 +8,17 @@
 </template>
 
 <style lang="less" scoped>
+
+.content {
+    padding: 0 20px;
+}
 .data {
-    margin: 20px 0 0 20px;
+    margin-top: 20px;
     user-select: none;
     width: 400px;
+
+    @media screen and (max-width: 900px) {
+        width: 100%;
+    }
 }
 </style>
