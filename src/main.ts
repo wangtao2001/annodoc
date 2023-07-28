416a02a059d50aaa2bf2a19f1b0f6a4f84ab205b
wangtao <wangtao.cpu@gmail.com>
2023-07-28 16:06:21 +0800

    feta: 502 page

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,14 @@ import axios  from 'axios'
 
 const init = async() => {
     axios.defaults.withCredentials = true // 请求携带cookie
+    // 502判断
+    axios.interceptors.response.use((response) => {
+        return response
+    }, (error)  => {
+        window.location.href = '/502.html'
+        return Promise.reject(error)
+    })
+
     const res = await axios.get("/api/auth/login")
     if (res.data.code == '70020') {
         window.location.href = res.data.data
